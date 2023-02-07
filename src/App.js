3d2d450ec524519abb9bf95d8126e3b1c12cb649
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-02-07 10:09:06 +0100

    Component title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Title from './components/title/title';
 function App() {
   return (
     <div>
-      <Title/>
+      <Title _param1={'Bachir'} _param2={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, minima?'}/>
     </div>
   );
 }
--- src/components/title/title.js
+++ src/components/title/title.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './public/css/app.css';
 
-const Title = () => {
+
+// ## Crée deux components :
+//     - Title
+//     - Picture
+
+// ### Dans le component Title, crée une function avec deux paramètres
+// * Dans cette function, return un h1 avec le premier paramètre et un p avec le deuxième paramètre (n'oublie pas d'exporter la function)
+// * Import ta function dans l'App et donne des valeurs au deux paramètres du component Title
+
+// ### Dans le component Picture, crée une function avec deux paramètres
+// * Dans cette function, return deux balises img avec comme src le paramètre
+// * Import le component dans l'App et donne les valeur aux paramètres (donne lui une valeur avec le lien url d'une image et l'autre avec le src d'une image dans le dossier img)
+
+const Title = ({_param1, _param2}) => {
   return (
-    <div>title</div>
+    <div>
+        <h1 className="text-3xl font-bold underline">{_param1}</h1>
+        <p>{_param2}</p>
+    </div>
+    
   )
 }
 
